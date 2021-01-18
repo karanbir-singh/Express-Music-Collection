8c6328b9a9e53ba88711640933f0da7ecbdbe64b
karanbir-singh <karans1723@gmail.com>
2021-01-18 17:30:52 +0100

    Completed all queries

--- index.js
+++ index.js
@@ -7,43 +7,81 @@ const app = express();
 const hostname = 'localhost';
 const port = 3000;
 
-//* Collezione di dischi
-const disks = [
-    { id: 1, title: "Sgt. Pepper's Lonely Hearts Club Band" }, //The Beatles
-    { id: 2, title: "Revolver" }, //The Beatles
-    { id: 3, title: "Exile on Main St." }, //The Rolling Stones
-    { id: 4, title: "Led Zeppelin" }, //Led Zeppelin
-    { id: 5, title: "Let It Bleed" }, //The Rolling Stones
-    { id: 6, title: "The Rise and Fall of Ziggy Stardust and the Spiders from Mars" }, //David Bowie
-    { id: 7, title: "Never Mind the Bollocks, Here's the Sex Pistols " }, //Sex pistols
-    { id: 8, title: "The Dark Side of the Moon" }, //Pink Floyd
-    { id: 9, title: "Bitches Brew" }, //Miles Davis
-    { id: 10, title: "A love supreme" } //John Coltrane
+//* Collezioni
+const data = [
+    {
+        diskId: 1,
+        title: "Sgt. Pepper's Lonely Hearts Club Band",
+        genre: {
+            id: 1,
+            type: "Pop rock"
+        },
+        author: {
+            id: 1,
+            name: "The Beatles"
+        },
+    },
+    {
+        diskId: 2,
+        title: "Exile on Main St.",
+        genre: {
+            id: 1,
+            type: "Pop rock"
+        },
+        author: {
+            id: 2,
+            name: "The Rolling Stones"
+        },
+    },
+    {
+        diskId: 3,
+        title: "Led Zeppelin",
+        genre: {
+            id: 2,
+            type: "Hard rock"
+        },
+        author: {
+            id: 3,
+            name: "Led Zeppelin"
+        },
+    },
+    {
+        diskId: 4,
+        title: "The Rise and Fall of Ziggy Stardust and the Spiders from Mars",
+        genre: {
+            id: 3,
+            type: "Punk rock"
+        },
+        author: {
+            id: 4,
+            name: "David Bowie"
+        },
+    },
+    {
+        diskId: 5,
+        title: "Bitches Brew",
+        genre: {
+            id: 4,
+            type: "Jazz"
+        },
+        author: {
+            id: 5,
+            name: "Miles Davis"
+        },
+    },
+    {
+        diskId: 6,
+        title: "A love supreme",
+        genre: {
+            id: 4,
+            type: "Jazz"
+        },
+        author: {
+            id: 6,
+            name: "John Coltrane"
+        },
+    },
 ];
-
-//* Collezione di autori
-const authors = [
-    { id: 1, name: "The Beatles" },
-    { id: 2, name: "The Rolling Stones" },
-    { id: 3, name: "Led Zeppelin" },
-    { id: 4, name: "David Bowie" },
-    { id: 5, name: "Sex pistols" },
-    { id: 6, name: "Pink Floyd" },
-    { id: 7, name: "Miles Davis" },
-    { id: 8, name: "John Coltrane" }
-];
-
-//* Collezione di generi
-const genres = [
-    { id: 1, type: "Pop rock" }, 
-    { id: 2, type: "Hard rock" }, 
-    { id: 3, type: "Country rock" }, 
-    { id: 4, type: "Glam rock" },
-    { id: 5, type: "Punk rock" },
-    { id: 6, type: "Jazz" },];
-
-//* Insieme di tutto
-
 
 //> Root: ritorna un oggetto json con esempi di possibili route a cui il server risponde.
 app.get('/', (req, res) => {
@@ -52,31 +90,42 @@ app.get('/', (req, res) => {
 
 //> Elenco di tutti i dischi: `GET /vinyls`
 app.get('/vinyls', (req, res) => {
-    res.send(disks);
+    const disks = data.map((disk) => { return disk.title });
+    res.send({ disks });
 });
 
 //> Elenco degli autori: `GET /authors`
 app.get('/authors', (req, res) => {
-    res.send(authors);
+    const authors = data.map((disk) => { return disk.author.name });
+    res.send({ authors });
 });
 
 //> Elenco dei generi: `GET /genres`
 app.get('/genres', (req, res) => {
-    res.send(genres);
+    const genres = data.map((disk) => { return disk.genre.type });
+    res.send({ genres });
 });
 
 //> Recupera informazioni su un disco: `GET /vinyls/:id` 
-app.get('/vinyls/:id', (req, res) => {
-    const { id } = req.params;
-
-    // TODO: recupero dettagli vinile richiesto
-
-    //Ritorno valori al cliente
-    res.send({});
+app.get('/vinyls/:diskId', (req, res) => {
+    const { diskId } = req.params;
+    const disk = data.filter((disk) => { return disk.diskId == diskId; });
+    res.send(...disk);
 });
 
 //> Recupera tutti i dischi di un autore: `GET /authors/:authorID/vinyls`
-//> Recupera i dischi di un genere: `GET /genres/:id/vinyls` 
+app.get('/authors/:authorId/vinyls', (req, res) => {
+    const { authorId } = req.params;
+    const disks = (data.filter((disk) => { return disk.author.id == authorId; })).map((x) => { return x.title });
+    res.send({ disks });
+});
+
+//> Recupera i dischi di un genere: `GET /genres/:id/vinyls`
+app.get('/genres/:genreId/vinyls', (req, res) => {
+    const { genreId } = req.params;
+    const disks = (data.filter((disk) => { return disk.genre.id == genreId; })).map((x) => { return x.title });
+    res.send({ disks });
+}); 
 
 //> Server in ascolto...
 app.listen(port, () => {
